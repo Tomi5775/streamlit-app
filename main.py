c08ceb7b4f17d03d57ec78eb89b7ee5312cc7c10
Tomi5775 <211220103@unmuhpnk.ac.id>
2025-01-27 01:33:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
