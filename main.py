a72424c6576390c4b9d9e58ca433e32720801f50
Tomi5775 <211220103@unmuhpnk.ac.id>
2025-01-27 01:59:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ data = pd.read_csv(data_path)
 
 # Streamlit app
 def main():
-    st.set_page_config(page_title="Prediksi Biaya Asuransi Kesehatan dengan Regresi Linear dan Random Forest", page_icon="💰", layout="wide")
+    st.title("Prediksi Biaya Asuransi Kesehatan dengan Regresi Linear dan Random Forest", page_icon="💰", layout="wide")
     
     # Menampilkan penjelasan tujuan aplikasi
     st.markdown("""
